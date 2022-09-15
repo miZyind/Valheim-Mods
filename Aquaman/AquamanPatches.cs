e491a752ed3274026e6d44917f799c942ac1b9d9
miZyind <mizyind@gmail.com>
2022-09-15 23:46:33 +0700

    init: Valheim-Mods project

---
+++ Aquaman/AquamanPatches.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using BepInEx;
+using HarmonyLib;
+
+namespace Aquaman
+{
+    [BepInPlugin("miZyind.Aquaman", "Aquaman", "2020.09.15")]
+    public class AquamanPatches : BaseUnityPlugin
+    {
+        private static List<string> allowedItems = new List<string> { "$item_hammer" };
+
+        private void Awake()
+        {
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        [HarmonyPatch(typeof(Character), "IsSwiming")]
+        public static class Character_IsSwiming_Patch
+        {
+            public static bool Prefix(Humanoid __instance, float ___m_swimTimer)
+            {
+                if (___m_swimTimer < 0.5f)
+                {
+                    if (__instance.IsPlayer())
+                    {
+                        var stackTrace = new StackTrace();
+                        var stackNames = "";
+                        var tracingFrame = 2;
+
+                        while (tracingFrame < stackTrace.FrameCount && tracingFrame < 5)
+                        {
+                            stackNames += stackTrace.GetFrame(tracingFrame).GetMethod().Name;
+                            tracingFrame++;
+                        }
+
+                        if (stackNames.Contains("UpdateEquipment") || stackNames.Contains("EquipItem"))
+                        {
+                            var rightItem = __instance.GetRightItem();
+                            var leftItem = __instance.GetLeftItem();
+
+                            if (
+                                rightItem != null && !allowedItems.Contains(rightItem.m_shared.m_name) ||
+                                leftItem != null && !allowedItems.Contains(leftItem.m_shared.m_name)
+                            )
+                            {
+                                __instance.HideHandItems();
+                            }
+                            else
+                            {
+                                return false;
+                            }
+
+                        }
+                    }
+
+                    return true;
+                }
+
+                return false;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,38 @@
+﻿# miZyind's Valheim Mods
+
+[![Valheim](https://img.shields.io/badge/valheim-000?style=for-the-badge&logo=steam)](https://store.steampowered.com/app/892970/Valheim)
+[![Unity](https://img.shields.io/badge/unity-000?style=for-the-badge&logo=unity)](https://unity.com)
+[![Visual Studio](https://img.shields.io/badge/2022-5c2d91?style=for-the-badge&logo=visual-studio)](https://visualstudio.microsoft.com)
+[![.NET](https://img.shields.io/badge/4.8.0-5c2d91?style=for-the-badge&logo=.net)](https://dotnet.microsoft.com)
+[![C#](https://img.shields.io/badge/7.3-239120?style=for-the-badge&logo=c-sharp)](https://docs.microsoft.com/dotnet/csharp)
+
+## 🔮 Tested Game Version
+
+- **0.210.6**
+
+## 🙏 Thanks To
+
+- @aedenthorn for [ValheimMods](https://github.com/aedenthorn/ValheimMods)
+
+## 💠 Development Environment & Tools
+
+- dnSpy
+- Visual Studio 2022
+- .NET Framework = 4.8.0
+- Unity = 2022.3.33
+- BepInEx = 5.4.19
+- HarmonyX = 2.9.0
+
+## 🗂 Mod List
+
+|  Name   |                   Description                   |
+| :-----: | :---------------------------------------------: |
+| Aquaman | Using equipment in water, just like an Aquaman! |
+
+## 🖋 Author
+
+miZyind <mizyind@gmail.com>
+
+## 📇 License
+
+Licensed under the [MIT](LICENSE) license.
